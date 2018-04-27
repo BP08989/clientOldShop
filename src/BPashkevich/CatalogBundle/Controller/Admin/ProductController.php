816edbca9a1234fe9ea322044a8069139d8f5b63
BP0898 <2kepocuhbl4a@gmail.com>
2018-04-27 23:04:57 +0300

    add some admin pages for managment store. Delete entities. Add sorting. Some fixes

--- src/BPashkevich/CatalogBundle/Controller/Admin/ProductController.php
+++ src/BPashkevich/CatalogBundle/Controller/Admin/ProductController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use BPashkevich\CatalogBundle\Helper\Methods;
 
-class DefaultController extends Controller
+class ProductController extends Controller
 {
     public function indexAction(Request $request)
     {
-        return $this->render('admin/home.html.twig');
+        $products = Methods::sendRequest('http://127.0.0.1:8000/api/getALLProducts');
+        return $this->render('admin/product/index.html.twig', array(
+            'products' => $products
+        ));
     }
 }
